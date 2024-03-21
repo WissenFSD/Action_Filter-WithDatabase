140d55bed675e65ce5a452e2bd3a70f6cd8fc86c
BRIGHT <BRIGHT@DESKTOP-5789TGK>
2024-03-21 19:43:10 +0300

    Add project files.

---
+++ Action_Filter/ActionFilter/CustomLoggingActionFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Action_Filter.ActionFilter
+{
+    public class CustomLoggingActionFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Action Filter, bir action'a girilmeden önce ve girildikten sonra çalışır.
+
+            //OnActionExecuting metodu  : action'dan önce 
+            //OnActionExecuted metodu    : actiondan sonra çalışır.
+
+
+            string controller = context.RouteData.Values["controller"].ToString();
+            string action = context.RouteData.Values["action"].ToString();
+
+            // Bu metotların çalışması için, Actionların üstüne yada controllerin üstüne bu action tanımları yapılması gerekmektedir.
+
+            //string controller = context.RouteData.Values.Values[0].;
+
+
+
+
+            // string action = context.RouteData.Values[1];
+            StreamWriter wri = new StreamWriter("c:\\log\\log.txt", true);
+            wri.WriteLine(controller+" içerisindeki "+action+" isimli metot çağırıldı");
+            wri.Close();
+            base.OnActionExecuting(context);
+        }
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+    }
+}
---
+++ Action_Filter/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Action_Filter.ActionFilter;
+using Action_Filter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Action_Filter.Controllers
+{
+    // CustomLogging Action Filter bu controler seviyesinde girilen tüm Actionları,Action Filterli bir biçimde ayarlayacaktır.i
+    [CustomLoggingActionFilter]
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        
+        }
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
